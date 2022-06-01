9118b5ca2befb0afa8eb754c30f0ba89f4b99f0f
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-06-01 15:16:47 -0300

    create Home page layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,36 @@
 import styles from './home.module.scss';
+import cru from '../../images/teams/cruzeiro.png';
+import crb from '../../images/teams/crb.png';
+import vas from '../../images/teams/vasco.png';
+import gre from '../../images/teams/gremio.png';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div className={ styles.home }>
+      <h2>Campeonato Brasileiro <br />Série B</h2>
+      <h3>Escolha a partida</h3>
+
+      <h4>Amanhã - 02/06</h4>
+      <div className={ styles.partidas }>
+        <ul>
+          <a href="/">
+            <li>
+              <img src={ vas } alt="Vasco" /> Vasco x Grêmio <img src={ gre } alt="Grêmio" />
+            </li>
+          </a>
+        </ul>
+      </div>
+      <h4>Sexta - 03/06</h4>
+      <div className={ styles.partidas }>
+        <ul>
+          <a href="/">
+            <li>
+              <img src={ crb } alt="CRB" /> CRB x Cruzeiro <img src={ cru } alt="Cruzeiro" />
+            </li>
+          </a>
+        </ul>
+      </div>
+    </div>
   );
 }
 
